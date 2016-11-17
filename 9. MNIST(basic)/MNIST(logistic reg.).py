a6a9a45c448023614b992460377b173605aa66da
gicheonkang <kgc519@gmail.com>
2016-11-18 00:27:19 +0900

    MNIST basic

---
+++ 9. MNIST(basic)/MNIST(logistic reg.).py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import input_data
+
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+hypothesis = tf.nn.softmax(tf.matmul(X, W)+b)
+cost = tf.reduce_mean(tf.reduce_sum(-Y*tf.log(hypothesis), reduction_indices=1))
+
+optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
+init = tf.initialize_all_variables()
+
+training_epoch = 5
+display_step = 1
+batch_size = 100
+checkpoint_dir = "cps/"
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+with tf.Session() as session:
+    session.run(init)
+
+    for epoch in range(training_epoch):
+        avg_cost = 0.
+        total_batch = int(mnist.train.num_examples / batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # Fit training using batch data
+            session.run(optimizer, feed_dict={X: batch_xs, Y: batch_ys})
+            # Compute average loss
+
+        # show logs per epoch step
+        avg_cost += session.run(cost, feed_dict={X: batch_xs, Y: batch_ys}) / total_batch
+        if epoch % display_step == 0:  # Softmax
+            print ("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+            print (session.run(b))
+
+
+    print ("Optimization Finished!")
+
+    # Test model
+    correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print ("Accuracy:", accuracy.eval({X: mnist.test.images, Y: mnist.test.labels}))
+
+
+
+
+
+
+
+
+
+
