2a8e3e783bc098e711453596dde4ab5d815ff602
gicheonkang <kgc519@gmail.com>
2016-11-18 10:06:22 +0900

    MNIST error-rate 2%

--- 9. MNIST(basic)/MNIST(NN).py
+++ 9. MNIST(basic)/MNIST(NN).py
@@ -12,6 +12,12 @@ def xavier_init(n_inputs, n_outputs, uniform = True):
 
 X = tf.placeholder(tf.float32, [None, 784])
 Y = tf.placeholder(tf.float32, [None, 10])
+dropout_rate = tf.placeholder(tf.float32)
+
+training_epoch = 15
+display_step = 1
+batch_size = 100
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 W1 = tf.get_variable("W1", shape=[784, 500], initializer=xavier_init(784,500))
 W2 = tf.get_variable("W2", shape=[500, 256], initializer=xavier_init(500,256))
@@ -23,18 +29,17 @@ b2 = tf.Variable(tf.zeros([256]))
 b3 = tf.Variable(tf.zeros([128]))
 b4 = tf.Variable(tf.zeros([10]))
 
-L2 = tf.nn.relu(tf.add(tf.matmul(X, W1),b1))
-L3 = tf.nn.relu(tf.add(tf.matmul(L2, W2),b2))
-L4 = tf.nn.relu(tf.add(tf.matmul(L3, W3),b3))
+_L2 = tf.nn.relu(tf.add(tf.matmul(X, W1),b1))
+L2 = tf.nn.dropout(_L2, dropout_rate)
+_L3 = tf.nn.relu(tf.add(tf.matmul(L2, W2),b2))
+L3 = tf.nn.dropout(_L3, dropout_rate)
+_L4 = tf.nn.relu(tf.add(tf.matmul(L3, W3),b3))
+L4 = tf.nn.dropout(_L4, dropout_rate)
 hypothesis = tf.add(tf.matmul(L4, W4), b4)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(hypothesis, Y))
-optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
+optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
 
-training_epoch = 15
-display_step = 1
-batch_size = 100
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 init = tf.initialize_all_variables()
 
@@ -48,11 +53,11 @@ with tf.Session() as session:
         for i in range(total_batch):
             batch_xs, batch_ys = mnist.train.next_batch(batch_size)
             # Fit training using batch data
-            session.run(optimizer, feed_dict={X: batch_xs, Y: batch_ys})
+            session.run(optimizer, feed_dict={X: batch_xs, Y: batch_ys, dropout_rate:0.7})
             # Compute average loss
 
         # show logs per epoch step
-        avg_cost += session.run(cost, feed_dict={X: batch_xs, Y: batch_ys}) / total_batch
+        avg_cost += session.run(cost, feed_dict={X: batch_xs, Y: batch_ys, dropout_rate:0.7}) / total_batch
         if epoch % display_step == 0:  # Softmax
             print ("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
             print (session.run(b4))
@@ -64,5 +69,5 @@ with tf.Session() as session:
     correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print ("Accuracy:", accuracy.eval({X: mnist.test.images, Y: mnist.test.labels}))
+    print ("Accuracy:", accuracy.eval({X: mnist.test.images, Y: mnist.test.labels, dropout_rate:1}))
 
